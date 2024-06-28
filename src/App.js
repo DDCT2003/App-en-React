07452f86f38c5e90fe0ab526c9dfa7030743de73
DDCT2003 <dylanclerque_2003@hotmail.com>
2024-06-27 22:47:51 -0500

    Primer commit:)

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hola!</h1>
+        <p>Este es un ejemplo de como funciona React para la clase de Diseño y arquitectura de software.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
